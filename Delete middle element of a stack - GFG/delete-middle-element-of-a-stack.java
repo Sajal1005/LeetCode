22031f856f61345e64eda8f8ec91a4829052e0af
Sajal Nanda <93420995+Sajal1005@users.noreply.github.com>
2023-07-09 21:42:36 +0530

    Added solution - LeetHub

--- Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.java
+++ Delete middle element of a stack - GFG/delete-middle-element-of-a-stack.java
@@ -46,12 +46,7 @@ class Solution
     public void deleteMid(Stack<Integer>s,int sizeOfStack){
         // code here
         Stack<Integer> temp = new Stack<>();
-        int n;
-        if(s.size()%2==0){
-            n=s.size()/2;
-        }else{
-            n=s.size()/2;
-        }
+        int n=s.size()/2;
         for(int i=0;i<n;i++){
            temp.push(s.pop()); 
         }
