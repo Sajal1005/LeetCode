5acb37463d5808acc9791aafa19a3390239b23c1
Sajal Nanda <93420995+Sajal1005@users.noreply.github.com>
2023-07-21 15:21:34 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.java
+++ Insertion Sort - GFG/insertion-sort.java
@@ -47,7 +47,7 @@ class Solution
   {
       //code here
       for(int i=1;i<n;i++){
-          int key =arr[i];
+          int key=arr[i];
           int j=i-1;
           while(j>=0 && arr[j]>key){
               arr[j+1]=arr[j];
