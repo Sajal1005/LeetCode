872d7344116cd8a729ace3e2cd28fe0e32d1fcc5
Sajal Nanda <93420995+Sajal1005@users.noreply.github.com>
2023-10-06 21:22:27 +0530

    Added solution - LeetHub

---
+++ Smallest number on left - GFG/smallest-number-on-left.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int n = Integer.parseInt(in.readLine());
+            String a1[] = in.readLine().trim().split("\\s+");
+            int a[] = new int[n];
+            for(int i = 0;i < n;i++)
+                a[i] = Integer.parseInt(a1[i]);
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.leftSmaller(n, a);
+            for(int i = 0;i < n;i++)
+                System.out.print(ans.get(i)+" ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static List<Integer> leftSmaller(int n, int arr[])
+    {
+        //code here
+        Stack<Integer> s = new Stack<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        s.push(-1);
+        for(int i=0;i<arr.length;i++){
+            while(s.peek()>=arr[i]){
+                s.pop();
+            }
+            ans.add(s.peek());
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+}
